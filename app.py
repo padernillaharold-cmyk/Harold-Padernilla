0cd633c30e1bedc56002c9e5b7b6b9cbaf527d77
padernillaharold-cmyk <padernillaharold@isufst.edu.ph>
2026-09-20 15:07:55 +0800

    Add new course and welcome routes to API

--- app.py
+++ app.py
@@ -19,6 +19,23 @@ def say_hello():
     return jsonify({
 "message": f"Hello, {name}!"
 })
+
+@app.route('/course')
+def get_course():
+    return jsonify({
+        "course_code": "IT 3120",
+        "course_name": "System Integration",
+        "program": "BSIT",
+        "year_level": 3
+})
+
+@app.route('/Welcome')
+def say_Welcome():
+    name = request.args.get('name', 'Student')
+    return jsonify({
+"message": f"Welcome, {name}! "
+})
+
 if __name__ == '__main__':
     app.run(debug=True)
 
